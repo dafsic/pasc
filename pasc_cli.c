2f011f84a34af0aaa761cac2b75d8063487d68de
ZhaoKaiqiang <dafsic@gmail.com>
2015-03-29 14:41:22 +0800

    use getopt parse command-line options instead of getopt_long,means only support short option.please use -h instead of --help for help.

--- pasc_cli.c
+++ pasc_cli.c
@@ -19,9 +19,6 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
-#include <getopt.h>
-
-static int verbose_flag;
 
 void usage();
 void pasc_connect(char *ipaddr);
@@ -33,67 +30,58 @@ void pasc_update(char *name);
 
 int main ( int argc, char *argv[] )
 {
-    int c;
-	int option_index = 0;
-    static struct option long_options[]=
-    {
-	{"connect",required_argument,0,'c'},
-	{"query",required_argument, 0,'q'},
-	{"join",required_argument,0,'j'},
-	{"login",required_argument,0,'l'},
-	{"exit",required_argument,0,'e'},
-	{"update",required_argument,0,'u'},
-	{"help",no_argument,0,'h'},
-	{0,0,0,0}
-    };
 
-    while((c = getopt_long(argc,argv,"hc:q:j:l:e:u:",long_options,&option_index)) != -1)
-    {
-		switch(c)
-			{
-			case 'c':
-			//if(optarg)
+	char opt;
+    if (argc <= 1 || argc >= 4)
+	{
+		usage();
+		return 1;
+	}
+	
+    opt = getopt(argc,argv,"hc:q:j:l:e:u:");
+	switch(opt)
+		{
+		case 'c':
+		//if(optarg)
 			pasc_connect(optarg);
 			break;
 
-			case 'q':
+		case 'q':
 			pasc_query(optarg);
 			break;
 
-			case 'j':
+		case 'j':
 			pasc_join(optarg);
 			break;
-			
-			case 'l':
+		
+		case 'l':
 			pasc_login(optarg);
 			break;
 
-			case 'e':
+		case 'e':
 			pasc_exit(optarg);
 			break;
-			
-			case 'u':
+		
+		case 'u':
 			pasc_update(optarg);
 			break;
 
-			case '?':
-			case 'h':
+		case 'h':
+		case '?':
+
+		default:
 			usage();
 			break;
+		}	
 
-			default:
-			printf("default\n");
-			abort();
-		}
-    }
-
-/*    if(optind < argc)
+/* 
+    if(optind < argc)
 	{
 		printf("non-option ARGV-elements:");
 		while(optind < argc)
 		printf("%s",argv[optind++]);
 		printf("\n");
-	}*/
+	} */
 
     return EXIT_SUCCESS;
 }
